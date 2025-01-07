c6c5cc1ff7c9fba3b56dc4866583d524db8c34ac
priyansh0202-coder <139114935+priyansh0202-coder@users.noreply.github.com>
2025-01-07 15:48:34 +0530

    Update Login.jsx

--- src/app/login/Login.jsx
+++ src/app/login/Login.jsx
@@ -36,7 +36,7 @@ const Login = () => {
                 position: "top-center"
             })
             context.setUser(result.user)
-            router.push("/api/current")
+            router.push("/profile/user")
 
         } catch (error) {
             console.log(error)
